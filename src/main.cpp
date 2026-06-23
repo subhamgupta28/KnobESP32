92d1c3f627333477b6c882f2c1e46e91ca464fc4
Subham gupta <67224573+subhamgupta28@users.noreply.github.com>
2026-06-24 02:03:46 +0530

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,154 +1,154 @@
 #include <Arduino.h>
-#include "Automata.h"
-#include "ArduinoJson.h"
-#include <WiFi.h>
-#include <Arduino.h>
-#include "bidi_switch_knob.h"
-#include <HardwareSerial.h>
+// #include "Automata.h"
+// #include "ArduinoJson.h"
+// #include <WiFi.h>
+// #include <Arduino.h>
+// #include "bidi_switch_knob.h"
+// #include <HardwareSerial.h>
 
-const char *HOST = "raspberry.local";
-int PORT = 8010;
-Preferences preferences;
-Automata automata("KNOB_NOM", HOST, PORT);
-HardwareSerial SerialPort(1);
-long d = 8000;
-long st = millis();
-unsigned long startMillis;
-int ch = millis();
-long start = millis();
-JsonDocument doc;
+// const char *HOST = "raspberry.local";
+// int PORT = 8010;
+// Preferences preferences;
+// Automata automata("KNOB_NOM", HOST, PORT);
+// HardwareSerial SerialPort(1);
+// long d = 8000;
+// long st = millis();
+// unsigned long startMillis;
+// int ch = millis();
+// long start = millis();
+// JsonDocument doc;
 
-// Knob
-#define EXAMPLE_ENCODER_ECA_PIN 19
-#define EXAMPLE_ENCODER_ECB_PIN 22
-#define ESP32_RX 18
-#define ESP32_TX 23
+// // Knob
+// #define EXAMPLE_ENCODER_ECA_PIN 19
+// #define EXAMPLE_ENCODER_ECB_PIN 22
+// #define ESP32_RX 18
+// #define ESP32_TX 23
 
-#define SET_BIT(reg, bit) (reg |= ((uint32_t)0x01 << bit))
-#define CLEAR_BIT(reg, bit) (reg &= (~((uint32_t)0x01 << bit)))
-#define READ_BIT(reg, bit) (((uint32_t)reg >> bit) & 0x01)
-#define BIT_EVEN_ALL (0x00ffffff)
-EventGroupHandle_t knob_even_ = NULL;
-static knob_handle_t s_knob = 0;
-int encPos = 0;
-SemaphoreHandle_t mutex;
+// #define SET_BIT(reg, bit) (reg |= ((uint32_t)0x01 << bit))
+// #define CLEAR_BIT(reg, bit) (reg &= (~((uint32_t)0x01 << bit)))
+// #define READ_BIT(reg, bit) (((uint32_t)reg >> bit) & 0x01)
+// #define BIT_EVEN_ALL (0x00ffffff)
+// EventGroupHandle_t knob_even_ = NULL;
+// static knob_handle_t s_knob = 0;
+// int encPos = 0;
+// SemaphoreHandle_t mutex;
 
-void action(const Action action)
-{
-  String jsonString;
-  serializeJson(action.data, jsonString);
-  Serial.println(jsonString);
-}
-void sendData()
-{
-  automata.sendData(doc);
-}
-static void _knob_left_cb(void *arg, void *data)
-{
-  uint8_t eventBits_ = 0;
-  SET_BIT(eventBits_, 0);
-  xEventGroupSetBits(knob_even_, eventBits_);
-}
-static void _knob_right_cb(void *arg, void *data)
-{
-  uint8_t eventBits_ = 0;
-  SET_BIT(eventBits_, 1);
-  xEventGroupSetBits(knob_even_, eventBits_);
-}
+// void action(const Action action)
+// {
+//   String jsonString;
+//   serializeJson(action.data, jsonString);
+//   Serial.println(jsonString);
+// }
+// void sendData()
+// {
+//   automata.sendData(doc);
+// }
+// static void _knob_left_cb(void *arg, void *data)
+// {
+//   uint8_t eventBits_ = 0;
+//   SET_BIT(eventBits_, 0);
+//   xEventGroupSetBits(knob_even_, eventBits_);
+// }
+// static void _knob_right_cb(void *arg, void *data)
+// {
+//   uint8_t eventBits_ = 0;
+//   SET_BIT(eventBits_, 1);
+//   xEventGroupSetBits(knob_even_, eventBits_);
+// }
 
-bool shoot = false;
-int pos = -96;
-bool changeDetected = false;
+// bool shoot = false;
+// int pos = -96;
+// bool changeDetected = false;
 
-static void user_encoder_loop_task(void *arg)
-{
+// static void user_encoder_loop_task(void *arg)
+// {
 
-  for (;;)
-  {
-    EventBits_t even = xEventGroupWaitBits(knob_even_, BIT_EVEN_ALL, pdTRUE, pdFALSE, pdMS_TO_TICKS(50));
-    if (READ_BIT(even, 0))
-    {
-      if (xSemaphoreTake(mutex, portMAX_DELAY))
-      {
-        if (encPos > 0)
-          encPos = encPos - 5;
-        Serial.println("even");
-        changeDetected = true;
-        xSemaphoreGive(mutex);
-      }
-      // vibrateStrong2Sec();
-    }
-    if (READ_BIT(even, 1))
-    {
-      if (xSemaphoreTake(mutex, portMAX_DELAY))
-      {
-        encPos = encPos + 5;
-        if (encPos > 255)
-          encPos = 255;
-        Serial.println("odd");
-        changeDetected = true;
-        xSemaphoreGive(mutex);
-      }
-      // vibrateStrong2Sec();
-    }
-    vTaskDelay(100);
-  }
-}
-void initKnob()
-{
-  knob_even_ = xEventGroupCreate();
-  // create knob
-  knob_config_t cfg =
-      {
-          .gpio_encoder_a = EXAMPLE_ENCODER_ECA_PIN,
-          .gpio_encoder_b = EXAMPLE_ENCODER_ECB_PIN,
-      };
-  s_knob = iot_knob_create(&cfg);
+//   for (;;)
+//   {
+//     EventBits_t even = xEventGroupWaitBits(knob_even_, BIT_EVEN_ALL, pdTRUE, pdFALSE, pdMS_TO_TICKS(50));
+//     if (READ_BIT(even, 0))
+//     {
+//       if (xSemaphoreTake(mutex, portMAX_DELAY))
+//       {
+//         if (encPos > 0)
+//           encPos = encPos - 5;
+//         Serial.println("even");
+//         changeDetected = true;
+//         xSemaphoreGive(mutex);
+//       }
+//       // vibrateStrong2Sec();
+//     }
+//     if (READ_BIT(even, 1))
+//     {
+//       if (xSemaphoreTake(mutex, portMAX_DELAY))
+//       {
+//         encPos = encPos + 5;
+//         if (encPos > 255)
+//           encPos = 255;
+//         Serial.println("odd");
+//         changeDetected = true;
+//         xSemaphoreGive(mutex);
+//       }
+//       // vibrateStrong2Sec();
+//     }
+//     vTaskDelay(100);
+//   }
+// }
+// void initKnob()
+// {
+//   knob_even_ = xEventGroupCreate();
+//   // create knob
+//   knob_config_t cfg =
+//       {
+//           .gpio_encoder_a = EXAMPLE_ENCODER_ECA_PIN,
+//           .gpio_encoder_b = EXAMPLE_ENCODER_ECB_PIN,
+//       };
+//   s_knob = iot_knob_create(&cfg);
 
-  iot_knob_register_cb(s_knob, KNOB_LEFT, _knob_left_cb, NULL);
-  iot_knob_register_cb(s_knob, KNOB_RIGHT, _knob_right_cb, NULL);
-  xTaskCreate(user_encoder_loop_task, "user_encoder_loop_task", 3000, NULL, 2, NULL);
-}
+//   iot_knob_register_cb(s_knob, KNOB_LEFT, _knob_left_cb, NULL);
+//   iot_knob_register_cb(s_knob, KNOB_RIGHT, _knob_right_cb, NULL);
+//   xTaskCreate(user_encoder_loop_task, "user_encoder_loop_task", 3000, NULL, 2, NULL);
+// }
 
 void setup()
 {
-  mutex = xSemaphoreCreateMutex();
-  Serial.begin(115200);
-  delay(1000);
-  initKnob();
-  preferences.begin("bat", false);
-  automata.begin();
-  // automata.addAttribute("encoder1", "Encoder 1", "", "DATA|MAIN");
-  automata.addAttribute("screen", "Screen", "", "DATA|MAIN");
+  // mutex = xSemaphoreCreateMutex();
+  // Serial.begin(115200);
+  // delay(1000);
+  // initKnob();
+  // preferences.begin("bat", false);
+  // automata.begin();
+  // // automata.addAttribute("encoder1", "Encoder 1", "", "DATA|MAIN");
+  // automata.addAttribute("screen", "Screen", "", "DATA|MAIN");
 
-  automata.addAttribute("encoder", "Encoder", "", "DATA|MAIN");
-  automata.addAttribute("bright", "Brightness", "", "ACTION|MENU|SLIDER", doc);
-  automata.addAttribute("battery_volt", "Battery", "V", "DATA|AUX");
+  // automata.addAttribute("encoder", "Encoder", "", "DATA|MAIN");
+  // automata.addAttribute("bright", "Brightness", "", "ACTION|MENU|SLIDER", doc);
+  // automata.addAttribute("battery_volt", "Battery", "V", "DATA|AUX");
 
 
-  automata.registerDevice();
-  automata.onActionReceived(action);
-  automata.delayedUpdate(sendData);
-  SerialPort.begin(9600, SERIAL_8N1, ESP32_RX, ESP32_TX);
+  // automata.registerDevice();
+  // automata.onActionReceived(action);
+  // automata.delayedUpdate(sendData);
+  // SerialPort.begin(9600, SERIAL_8N1, ESP32_RX, ESP32_TX);
 }
 
 void loop()
 {
   // Serial.println("loop");
-  doc["encoder"] = encPos;
-  if (SerialPort.available())
-  {
-    String msg = SerialPort.readStringUntil('\n');
-    Serial.print("Received: ");
-    Serial.println(msg);
-    deserializeJson(doc, msg);
-  }
+  // doc["encoder"] = encPos;
+  // if (SerialPort.available())
+  // {
+  //   String msg = SerialPort.readStringUntil('\n');
+  //   Serial.print("Received: ");
+  //   Serial.println(msg);
+  //   deserializeJson(doc, msg);
+  // }
 
-  if (changeDetected || (millis() - start) > 1000)
-  {
-    automata.sendLive(doc);
-    start = millis();
-    // Start vibration
-    changeDetected = false;
-  }
+  // if (changeDetected || (millis() - start) > 1000)
+  // {
+  //   automata.sendLive(doc);
+  //   start = millis();
+  //   // Start vibration
+  //   changeDetected = false;
+  // }
 }
